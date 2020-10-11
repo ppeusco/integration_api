fcecf694a5f0eda64125585e389ce36fff64ca9b
Pablo Peuscovich <pp@qbkconsulting.com>
2020-10-11 14:50:25 -0300

    Make a Clients Controller

---
+++ app/controllers/concerns/exception_handler.rb
@@ -0,0 +1,11 @@
+# app/controllers/concerns/exception_handler.rb
+module ExceptionHandler
+  # provides the more graceful `included` method
+  extend ActiveSupport::Concern
+
+  included do
+    rescue_from ActiveRecord::RecordNotFound do |e|
+      json_response({ message: e.message }, :not_found)
+    end
+  end
+end
--- spec/factories/clients.rb
+++ spec/factories/clients.rb
@@ -1,12 +1,13 @@
 FactoryBot.define do
   factory :client do
-    email { "MyString" }
-    first_name { "MyString" }
-    last_name { "MyString" }
-    job { "MyString" }
-    country { "MyString" }
-    address { "MyString" }
-    zip_code { "MyString" }
-    phone { "MyString" }
+    id { Faker::Alphanumeric.alphanumeric(number: 32) }
+    email { Faker::Internet.safe_email }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    job { Faker::Job.title }
+    country { Faker::Address.country }
+    address { Faker::Address.street_address }
+    zip_code { Faker::Address.zip_code }
+    phone { Faker::PhoneNumber.phone_number }
   end
 end
