f2318c170a7306d531abb0bbef20dbdf0ef4acb6
Pablo Peuscovich <pp@qbkconsulting.com>
2021-02-15 16:12:38 -0300

    Config Sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
+require 'sidekiq/web'
+require 'sidekiq-scheduler/web'
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :clients, only: [:show, :index] do
     resources :transactions, only: [:show, :index]
   end
+  
+  mount Sidekiq::Web => '/sidekiq'
 end
