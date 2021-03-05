0bd2d7ef9d2212ce2610d1d38da2f83f73859549
Pablo Peuscovich <pp@qbkconsulting.com>
2021-03-05 15:24:19 -0300

    Rename jobs spec

--- spec/jobs/get_transactions_file_job_spec.rb
+++ spec/jobs/get_transactions_file_job_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe GetTransactionsJob, type: :job do
+RSpec.describe GetTransactionsFileJob, type: :job do
   pending "add some examples to (or delete) #{__FILE__}"
 end
