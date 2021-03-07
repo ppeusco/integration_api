090b34cb011dba187913901fe91d891fa60a943d
Pablo Peuscovich <pp@qbkconsulting.com>
2021-03-07 11:58:18 -0300

    Add CreateTrnsactionJob job

---
+++ app/jobs/create_transaction_job.rb
@@ -0,0 +1,12 @@
+class CreateTransactionJob < ApplicationJob
+  def perform(args = {})
+    transaction = args[:transaction]
+
+    Transaction.create!(
+      id: transaction['id'],
+      amount: transaction['amount'],
+      transaction_type: transaction['type'].to_i,
+      client_id: args[:client_id]
+    )
+  end
+end
