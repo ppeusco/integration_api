d336d6113ebf63b03dd7b5cbf61fad26f53d6f3a
Pablo Peuscovich <pp@qbkconsulting.com>
2021-03-07 12:06:14 -0300

    Add dot-env and interactor-rails gems

--- app/jobs/get_transactions_file_job.rb
+++ app/jobs/get_transactions_file_job.rb
@@ -1,9 +1,9 @@
 class GetTransactionsFileJob < ApplicationJob
   queue_as :transactions
   
-  def perform
-    oauth_token = '1234567890qwertyuiopasdfghjklzxcvbnm' 
-    client = TransactionApi::V1::Client.new(oauth_token)
-    transactions = client&.transaction_file&.split("\n")
+  def perform 
+    client = TransactionApi::V1::Client.new
+    transactions = client.transaction_file&.split("\n")
+    TransactionsManager::Importer.call(lines: transactions)
   end
 end
